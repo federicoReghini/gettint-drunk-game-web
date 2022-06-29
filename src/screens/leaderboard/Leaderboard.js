baa43d3690c51ae67a3acc6ad48475e5b90c5d77
Federico Reghini <federicoreghini@gmail.com>
2022-06-29 09:04:22 +0200

    new version

--- src/screens/leaderboard/Leaderboard.js
+++ src/screens/leaderboard/Leaderboard.js
@@ -1,10 +1,18 @@
 import { getUsers, LeaderBoardNf } from 'gettint-drunk';
-import React, { useEffect } from 'react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Leaderboard() {
 
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate('/Home')
+  }
+  const n = 123456;
+  console.log(n);
+
   return (
-    <LeaderBoardNf />
+    <LeaderBoardNf onClickNavigate={handleNavigate} />
   )
 }
 
