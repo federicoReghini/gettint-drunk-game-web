bad6cdc117afb9b477a969e75f0983d6ccdc8d28
Federico Reghini <federicoreghini@gmail.com>
2022-06-30 11:34:14 +0200

    new version game

--- src/screens/game/Game.js
+++ src/screens/game/Game.js
@@ -1,13 +1,21 @@
 import { LobbyContainer } from 'gettint-drunk'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { routes } from '../../routes/routes';
 
 function Game() {
 
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate(routes.HOME);
+  }
+
   return (
     <div>
-      <LobbyContainer />
+      <LobbyContainer onAfterQuit={handleNavigation} />
     </div>
   )
 }
 
-export default Game
+export default Game;
