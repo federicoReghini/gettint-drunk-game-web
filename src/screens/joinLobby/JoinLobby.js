bf23d89200747be40d66038faf1b2461f4111f7e
Federico Reghini <federicoreghini@gmail.com>
2022-07-04 12:01:36 +0200

    new version 1.1.6

--- src/screens/joinLobby/JoinLobby.js
+++ src/screens/joinLobby/JoinLobby.js
@@ -117,7 +117,7 @@ function JoinLobby() {
       connect(id);
 
       // if (lobby === null) {
-      editLobby(56,null,token).then(response => {
+      editLobby(59,null,token).then(response => {
         lobby = response?.data;
 
         eventOn('lobby', e => {
